17430bd72742e5a2a389a3c9ce03b1ebc4af86ea
Leonardo Duarte <hintl3rgames2015@gmail.com>
2022-10-18 18:37:52 -0300

    terminei o site

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+import Swiper, {
+    SwiperPluginLazyload,
+    SwiperPluginPagination
+} from 'tiny-swiper'
+
+Swiper.use([ SwiperPluginLazyload, SwiperPluginPagination ])
+
+const swiper = new Swiper(swiperContainer: HTMLElement | string, parameters?: TinySwiperParameters)
